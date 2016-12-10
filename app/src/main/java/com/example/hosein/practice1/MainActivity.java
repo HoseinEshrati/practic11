db8235eae1c27adeccbc53f48a698da4ee775faa
HOSEIN\Hosein <eshrati@outlook.com>
2016-12-10 10:18:12 +0330

    finish layout and start java cod

--- app/src/main/java/com/example/hosein/practice1/MainActivity.java
+++ app/src/main/java/com/example/hosein/practice1/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.hosein.practice1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ImageView imageView;
+    RadioGroup radioGroup;
+    RadioButton r1,r2,r3;
+    CheckBox checkBoxvisibility,checkBoxtarnsparent;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        
     }
 }
